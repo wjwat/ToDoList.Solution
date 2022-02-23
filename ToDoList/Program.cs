45820b33ef0b17f8377536b8ebd20efc10f18b79
Will W <wjwat@onionslice.org>
2022-02-23 11:23:12 -0600

    Add default cases to user input
    Co-authored-by: Anastasiia Riabets <anastasiia.ria@gmail.com>

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -14,7 +14,7 @@ namespace ToDoList
 
       while (true)
       {
-        cwl("Would you like to add an item to your list or view your list? (Add/View/Quit)");
+        cwl("Would you like to add an item to your list or view your list? (Add/View/Clear/Quit)");
         cw("ACTION: ");
 
         string action = Console.ReadLine().ToUpper();
@@ -22,14 +22,43 @@ namespace ToDoList
         switch (action)
         {
           case "ADD":
-            cwl("You've added something!\n");
+            cwl("Please enter the description for the new item:");
+            string description = Console.ReadLine();
+            Item item = new Item(description);
+            cw($"'{description}' has been added to your list. ");
             break;
           case "VIEW":
-            cwl("You're looking at stuff!\n");
+            int counter = 1;
+            cwl("ITEMS:");
+            foreach(Item i in Item.GetAll())
+            {
+              cwl($"    {counter}: " + i.Description);
+              counter += 1;
+            }
+            break;
+          case "CLEAR":
+            cw("Are you sure you'd like to clear your list? [YES/NO] ");
+            string choice = Console.ReadLine().ToUpper();
+            switch (choice)
+            {
+              case "YES":
+                Item.ClearAll();
+                cwl("Your items have been cleared.");
+                break;
+              case "NO":
+                cwl("Phew, that was a close one!");
+                break;
+              default:
+                cwl("I did not understand the command.");
+                break;
+            }
             break;
           case "QUIT":
             cwl("Have a great day!\n");
             return;
+          default:
+            cwl("I did not understand the command.");
+            break;
         }
       }
     }
